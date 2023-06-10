3eb1f0055ab706863921f4c1422f2a141f998638
Hector <torrezhectorb@gmail.com>
2023-06-09 23:01:53 -0600

    converts number to string

---
+++ 8 kyu/Convert-a-Number-to-a-String!.js
@@ -0,0 +1,13 @@
+// We need a function that can transform a number (integer) into a string.
+
+// What ways of achieving this do you know?
+
+// Examples (input --> output):
+// 123  --> "123"
+// 999  --> "999"
+
+
+const numbersToString= (num) => num.toString()
+console.log(numbersToString(-100)) /* --> "-100" */
+
+
