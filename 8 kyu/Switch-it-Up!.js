f578d3857640a3b68d9b0a2fc8d3ce7f3bb3219f
Hector <torrezhectorb@gmail.com>
2023-09-07 14:14:09 -0600

    Switch it Up!

---
+++ 8 kyu/Switch-it-Up!.js
@@ -0,0 +1,11 @@
+/* When provided with a number between 0 - 9, return it in words.
+
+    Input :: 1
+
+Output:: "One". */
+
+function switchItUp(number) {
+    //Write your own Code!
+    let arr = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine']
+    return arr[number]
+}
